202767253ab55a0fd84e2a1919796a5b74768e8c
kgashok <kgashok@users.noreply.github.com>
2016-05-06 14:14:30 +0530

    Update fizzBuzz.elm
    a dynamic version of FizzBuzz

--- elm/fizzBuzz.elm
+++ elm/fizzBuzz.elm
@@ -1,11 +1,72 @@
-import Graphics.Element exposing (Element, flow, down, show)
-import List exposing (map)
+import Html exposing (..) 
+import Graphics.Element exposing (..) 
+   
+type alias Filter = 
+  { divisor : Int, 
+    string : String
+  } 
 
-main : Element
+inputPairs = 
+  [
+    (3, "Fizz"), 
+    (5, "Buzz"), 
+    (7, "Mix")
+  ]
+
+createFilter : (Int, String)  -> Filter 
+createFilter tt =
+  let 
+    (x, s ) = tt 
+  in 
+    { divisor = x, string = s} 
+        
+buildFilterList pairs = 
+  pairs 
+    |> List.map (\x -> (fst x, snd x))
+    |> List.map createFilter
+    
+  
+applyFilter : Int -> Filter -> Maybe String 
+applyFilter number filter = 
+  let 
+    rem = (%) number filter.divisor
+    isMultiple = if rem == 0 then True else False 
+  in 
+    if isMultiple then 
+      Just filter.string
+    else 
+      Nothing 
+
+
+applyFilters listf number = 
+  let 
+    olist = List.filterMap (applyFilter number) listf 
+    res   = List.foldr (++) "" olist
+  in 
+    if res == "" then
+      toString number
+    else 
+      res
+
+
+main: Element
 main = 
-  let fizzBuzz n = case (n % 3, n % 5) of
-    (0, 0) -> "FizzBuzz"
-    (0, _) -> "Fizz"
-    (_, 0) -> "Buzz"
-    _      -> toString n
-  in map fizzBuzz [1..100] |> map show |> flow down
+  let 
+    f3      = createFilter (3, "Fizz" ) 
+    f5      = createFilter (5, "Buzz" )
+    f7      = createFilter (7, "Mix"  ) 
+    -- listF   = [f3, f5, f7]
+    listF   = buildFilterList inputPairs 
+    
+    result  = applyFilter 105 f3
+    result2 = List.filterMap (applyFilter 143) listF
+
+    out     = applyFilters listF 123
+    
+    finalo  = List.map (applyFilters listF) [1..110]
+    
+    finalF  = 
+      List.map (applyFilters listF) [1..110]
+   
+  in 
+    finalo |> List.map show |> flow down
