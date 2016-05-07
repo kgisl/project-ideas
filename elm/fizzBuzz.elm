f48f2a200d54b39174ef2021cfbe9da47598bf3d
kgashok <kgashok@users.noreply.github.com>
2016-05-07 23:14:20 +0530

    Update fizzBuzz.elm

--- elm/fizzBuzz.elm
+++ elm/fizzBuzz.elm
@@ -4,58 +4,37 @@ type alias Filter =
   { divisor : Int, 
     string : String
   } 
-inputPairs = 
-  [
-    (3, "Fizz"), 
-    (5, "Buzz"), 
-    (7, "Mix")
+
+inputFilters = 
+  [ Filter 3 "Fizz"
+  , Filter 5 "Buzz"
+  , Filter 7 "Mix"
   ]
-createFilter : (Int, String)  -> Filter 
-createFilter tt =
-  let 
-    (x, s ) = tt 
-  in 
-    { divisor = x, string = s} 
-        
-buildFilterList pairs = 
-  pairs 
-    |> List.map (\x -> (fst x, snd x))
-    |> List.map createFilter
-    
+
   
 applyFilter : Int -> Filter -> Maybe String 
 applyFilter number filter = 
+  if number % filter.divisor == 0 then
+    Just filter.string
+  else 
+    Nothing
+   
+
+applyFilters filters number = 
   let 
-    rem = (%) number filter.divisor
-    isMultiple = if rem == 0 then True else False 
-  in 
-    if isMultiple then 
-      Just filter.string
-    else 
-      Nothing 
-applyFilters listf number = 
-  let 
-    olist = List.filterMap (applyFilter number) listf 
-    res   = List.foldr (++) "" olist
+    res = List.filterMap (applyFilter number) filters 
+      |> List.foldr (++) ""
   in 
     if res == "" then
       toString number
     else 
       res
+
+
 main: Element
 main = 
-  let 
-    f3      = createFilter (3, "Fizz" ) 
-    f5      = createFilter (5, "Buzz" )
-    f7      = createFilter (7, "Mix"  ) 
-    -- listF   = [f3, f5, f7]
-    listF   = buildFilterList inputPairs 
-    
-    -- result  = applyFilter 105 f3
-    -- result2 = List.filterMap (applyFilter 143) listF
-    -- out     = applyFilters listF 123
+  let     
     output  = 
-      List.map (applyFilters listF) [1..110]
-   
+      List.map (applyFilters inputFilters) [1..110]   
   in 
     output |> List.map show |> flow down
