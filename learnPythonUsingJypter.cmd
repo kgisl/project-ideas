36fcc48ddcdd38ec1498a199c84985ee014cfc8f
kgashok <kgashok@users.noreply.github.com>
2017-07-14 17:26:55 +0530

    Create learnPythonUsingJypter.cmd

---
+++ learnPythonUsingJypter.cmd
@@ -0,0 +1,15 @@
+## Python for Data analysis 
+
+http://www.firstpythonnotebook.org/
+
+This textbook will guide you through an investigation of money in politics using data from the California Civic Data Coalition.
+
+You will learn just enough of the Python computer programming language to work with the pandas library, a popular open-source tool for analyzing data. The course will teach you how to use pandas to read, filter, join, group, aggregate and rank structured data.
+
+You will also learn how to record, remix and republish your analysis using the Jupyter Notebook, a browser-based application for writing code that is emerging as the standard for sharing reproducible research in the sciences.
+
+
+## Reference
+
+https://www.californiacivicdata.org/2017/07/12/first-python-notebook-at-sdsu/
+
